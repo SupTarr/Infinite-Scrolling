e645dd363df64db5f95df1b31841b686cc49b15b
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-12-13 15:16:25 +0700

    Add axios get request to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
-import { useState } from 'react'
+import React, { useState } from "react";
+import useBookSearch from "./useBookSearch";
 
 function App() {
+  const [query, setQuery] = useState("");
+  const [pageNumber, setPageNumber] = useState(1);
+
+  function handleSearch(e) {
+    setQuery(e.target.value);
+    setPageNumber(1);
+  }
+
+  useBookSearch(query, pageNumber);
   return (
     <>
-      <input type="text" />
-      <div>Test</div>
+      <input type="text" onChange={handleSearch} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/useBookSearch.jsx
+++ src/useBookSearch.jsx
@@ -3,7 +3,22 @@ import axios from "axios";
 
 export default function useBookSearch(query, pageNumber) {
   useEffect(() => {
-
+    let cancel;
+    axios({
+      method: "GET",
+      url: "http://openlibrary.org/search.json",
+      // q = search term and page = page number in the API
+      params: { q: query, page: pageNumber },
+      // Use the cancelToken to cancel the previous request
+      cancelToken: new axios.CancelToken((c) => (cancel = c)),
+    })
+      .then((res) => {
+        console.log(res.data);
+      })
+      .catch((e) => {
+        if (axios.isCancel(e)) return;
+      });
+    return () => cancel();
   }, [query, pageNumber]);
   return null;
 }
