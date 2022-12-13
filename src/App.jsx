a076eb8d35c6f8b53eb4533760eddcb235de51bd
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2022-12-13 15:48:26 +0700

    Finish infinite scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import useBookSearch from "./useBookSearch";
 
 function App() {
   const [query, setQuery] = useState("");
   const [pageNumber, setPageNumber] = useState(1);
+
+  const { loading, error, books, hasMore } = useBookSearch(query, pageNumber);
+
+  const observer = useRef();
+  const lastBookElementRef = useCallback(
+    (node) => {
+      if (loading) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting && hasMore) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [loading, hasMore]
+  );
 
   function handleSearch(e) {
     setQuery(e.target.value);
@@ -14,6 +31,19 @@ function App() {
   return (
     <>
       <input type="text" onChange={handleSearch} />
+      {books.map((book, index) => {
+        if (books.length === index + 1) {
+          return (
+            <div ref={lastBookElementRef} key={index}>
+              {book}
+            </div>
+          );
+        } else {
+          return <div key={index}>{book}</div>;
+        }
+      })}
+      <div>{loading && "Loading..."}</div>
+      <div>{error && "Error"}</div>
     </>
   );
 }
